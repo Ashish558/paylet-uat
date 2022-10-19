8c6774a1f4a5e3cac6f009f9643cfff21c20737b
Ankeeta Sahoo <sahooankeeta@gmail.com>
2022-10-19 21:12:27 +0530

    added login page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import "./css/bootstrap.min.css";
-import "./css/bootstrap.css";
-// import "./css/mdb.css";
+// import "./css/bootstrap.min.css";
+// import "./css/bootstrap.css";
+
 import "./css/style.css";
+import React from "react";
 import { Route, BrowserRouter, Redirect, Switch } from "react-router-dom";
-import { Header, Footer, Home } from "./components";
+import { Header, Footer, Home, Login } from "./components";
 const App = () => {
+  const [user, setUser] = React.useState(null);
+
   return (
     <BrowserRouter>
-      <Header />
+      {user && <Header />}
 
       <Switch>
+        <Route path="/login" exact component={Login} />
         <Route path="/" exact component={Home} />
       </Switch>
       <Footer />
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from "./header";
 export { default as Footer } from "./footer";
 export { default as Home } from "./home";
+export { default as Login } from "./login";
